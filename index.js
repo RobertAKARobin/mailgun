6b389bbc246dae3d86c746f6f8738bbdd0c2f289
Robert Thomas <robertgfthomas@gmail.com>
2016-10-20 21:47:46 -0400

    Fixed sending emails

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(function setHeaders(req, res, next){
   next();
 });
 app.use(function getEmail(req, res, next){
-  req.email = (req.query || req.body.contact || {}).email;
+  req.email = (req.query.email || (req.body.contact || {}).email);
   next();
 });
 app.use(function validateEmail(req, res, next){
@@ -31,9 +31,11 @@ app.use(function validateEmail(req, res, next){
   }
   request.get(params, function(error, response, body){
     var body = JSON.parse(body || "{}");
-    if(response.statusCode > 200 || !body.is_valid){
-      return report(res, "Something's wrong with your email.");
-    }else next();
+    if(body.is_valid){
+      next();
+    }else{
+      res.json({success: false, error: "Something's wrong with your e-mail."});
+    }
   });
 });
 
@@ -42,36 +44,31 @@ app.get("/", function(req, res){
 });
 
 app.post("/send", function(req, res){
-    var contact = req.body.contact;
-    var attachment = new mailer.Attachment({
-      data: new Buffer(JSON.stringify(req.body, null, 2), "utf8"),
-      filename: req.email + ".json.txt",
-      contentType: "text/plain"
+  var contact = req.body.contact;
+  var attachment = new mailer.Attachment({
+    data: new Buffer(JSON.stringify(req.body, null, 2), "utf8"),
+    filename: req.email + ".json.txt",
+    contentType: "text/plain"
+  });
+  var data = {
+    from: env.name + " <" + env.email + ">",
+    to: req.email,
+    cc: env.email,
+    subject: "Apps a la Carte!",
+    text: "Hi " + (contact.name || "there") + "! Looks like you just submitted the form at robertakarobin.com/apps_a_la_carte. I'll respond as soon as I can!",
+    attachment: attachment
+  };
+  try{
+    mailer.messages().send(data, function(error, body){
+      if(error) throw error;
+      else res.send({success: true});
     });
-    var data = {
-      from: env.name + " <" + env.email + ">",
-      to: req.email,
-      cc: env.email,
-      subject: "Apps a la Carte!",
-      text: "Hi " + (contact.name || "there") + "! Looks like you just submitted the form at robertakarobin.com/apps_a_la_carte. I'll respond as soon as I can!",
-      attachment: attachment
-    };
-    try{
-      mailer.messages().send(data, function(error, body){
-        if(error) throw error;
-        else res.send({success: true});
-      });
-    }catch(error){
-      return report(res, error);
-    }
+  }catch(error){
+    res.json({success: false, error: "Something went wrong! Try again later."});
   }
-);
+});
 
 app.listen("3002", function(){
   console.log("Mailer is running.");
 });
 
-function report(res, error){
-  console.log(error);
-  res.status(400).json({success:false, error: (error.message || error)});
-}
