300a63a6be7e58da91030efb48f5eb8a82637610
Robert Thomas <robertgfthomas@gmail.com>
2016-10-20 21:16:24 -0400

    Added validator

--- index.js
+++ index.js
@@ -10,60 +10,56 @@ var mailer = mailgun({
   domain: env.domain
 });
 
-app.use(function(req, res, next){
+app.use(bodyParser.json());
+app.use(function setHeaders(req, res, next){
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   // res.setHeader("Access-Control-Allow-Origin", "http://www.robertakarobin.com/apps_a_la_carte");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-app.use(bodyParser.json());
-
-app.get("/", function(req, res){
-  res.send("Not much here.");
+app.use(function getEmail(req, res, next){
+  req.email = (req.query || req.body.contact || {}).email;
+  next();
+});
+app.use(function validateEmail(req, res, next){
+  var params = {
+    url: "https://api.mailgun.net/v3/address/validate",
+    qs: {
+      address: req.email,
+      api_key: env.validation_key
+    }
+  }
+  request.get(params, function(error, response, body){
+    var body = JSON.parse(body || "{}");
+    if(response.statusCode > 200 || !body.is_valid){
+      return report(res, "Something's wrong with your email.");
+    }else next();
+  });
 });
 
-app.post("/send",
-  function validateEmail(req, res, next){
-    var contact = req.body.contact;
-    var params = {
-      url: "https://api.mailgun.net/v3/address/validate",
-      form: {
-        address: contact.email,
-        api_key: env.validation_key
-      }
-    }
-    request.get(params, function(error, response, body){
-      if(response.statusCode > 200){
-        return report(res, "Something's wrong with your email.");
-      }else next();
-    });
-  },
-  function prepEmail(req, res, next){
+app.get("/", function(req, res){
+  res.json({success: true, email: req.email});
+});
+
+app.post("/send", function(req, res){
     var contact = req.body.contact;
     var attachment = new mailer.Attachment({
       data: new Buffer(JSON.stringify(req.body, null, 2), "utf8"),
-      filename: contact.email + ".json.txt",
+      filename: req.email + ".json.txt",
       contentType: "text/plain"
     });
     var data = {
       from: env.name + " <" + env.email + ">",
-      to: contact.email,
+      to: req.email,
       cc: env.email,
       subject: "Apps a la Carte!",
       text: "Hi " + (contact.name || "there") + "! Looks like you just submitted the form at robertakarobin.com/apps_a_la_carte. I'll respond as soon as I can!",
       attachment: attachment
     };
-    req.email = data;
-    next();
-  },
-  function send(req, res){
     try{
-      mailer.messages().send(req.email, function(error, body){
-        if(error){
-          throw error;
-        }else{
-          res.send({success: true});
-        }
+      mailer.messages().send(data, function(error, body){
+        if(error) throw error;
+        else res.send({success: true});
       });
     }catch(error){
       return report(res, error);
